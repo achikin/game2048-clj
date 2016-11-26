096804bae98b87a3a1d14bb5671096ff4d1159d2
Anton Chikin <anton.m.chikin@gmail.com>
2016-11-26 12:24:21 +0300

    The whole file was duplicated thanks to copy&paste.

--- src/game2048/row.clj
+++ src/game2048/row.clj
@@ -1,49 +1,6 @@
 (ns game2048.row)
 
 (defmulti padd (fn [direction len coll] direction))
-(defmethod padd :left
-  [direction len coll]
-  (concat (repeat (- len (count coll)) 0) coll))
-(defmethod padd :right
-  [direction len coll]
-  (concat coll (repeat (- len (count coll)) 0)))
-  
-
-(defmulti merger(fn [dir & args] dir))
-
-(defmethod merger :left
-  ([dir [first second & rest] newrow]
-   (if first
-    (if (= first second)
-     (recur dir rest (cons (+ first second) newrow))
-     (recur dir (cons second rest) (cons first newrow)))
-    (reverse newrow)))
-  ([dir row]
-   (merger dir row '())))
-
-(defmethod merger :right
-  [dir row]
-  (reverse (merger :left (reverse row))))
-
-
-(defn remove-zeroes
-  "Return collection dropping all zeroes"
-  [coll]
-  (filter (fn [x] (not (= x 0))) 
-    coll))
-
-(defn opposite-dir
-  [dir]
-  (case dir
-    :left :right
-    :right :left))
-
-(defn move
-  [dir row]
-  (let [row-size (count row)]
-    (padd (opposite-dir dir) row-size (merger dir (remove-zeroes row)))))(ns game2048.row)
-
-(defmulti padd (fn [direction len coll] direction))
 "Pad collections with zeroes either on the left or on the right"
 (defmethod padd :left
   [direction len coll]
@@ -74,7 +31,7 @@ e.g. (merger :left '(1 1 2 2)) -> (2 4)"
 (defn remove-zeroes
   "Return collection dropping all zeroes"
   [coll]
-  (filter (fn [x] (not (= x 0))) 
+  (filter (fn [x] (not (zero? x))) 
     coll))
 
 (defn opposite-dir
