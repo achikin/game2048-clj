d51c9d15f6bf8c139e2ddfdded5b941c3b5ee94e
Anton Chikin <anton.m.chikin@gmail.com>
2017-02-08 18:46:03 +0300

    Updated nightlight

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-lanterna "0.9.4"]
-                 [nightlight "1.2.1"]]      
+                 [nightlight "1.6.1"]]      
   :main ^:skip-aot game2048.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
